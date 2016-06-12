b0eb5c9b7270c569e79ff64c5f838ca55a86d20f
Anatoly Chernysh <anatoly.chernysh@igniteoutsourcing.com>
2016-06-12 21:30:10 +0300

    A prime is a positive integer X that has exactly two distinct divisors: 1 and X. The first few prime integers are 2, 3, 5, 7, 11 and 13.
    A semiprime is a natural number that is the product of two (not necessarily distinct) prime numbers. The first few semiprimes are 4, 6, 9, 10, 14, 15, 21, 22, 25, 26.
    
    You are given two non-empty zero-indexed arrays P and Q, each consisting of M integers. These arrays represent queries about the number of semiprimes within specified ranges.
    
    Query K requires you to find the number of semiprimes within the range (P[K], Q[K]), where 1 ≤ P[K] ≤ Q[K] ≤ N.
    
    For example, consider an integer N = 26 and arrays P, Q such that:
    
        P[0] = 1    Q[0] = 26
        P[1] = 4    Q[1] = 10
        P[2] = 16   Q[2] = 20
    The number of semiprimes within each of these ranges is as follows:
    
    (1, 26) is 10,
    (4, 10) is 4,
    (16, 20) is 0.
    Write a function:
    
    class Solution { public int[] solution(int N, int[] P, int[] Q); }
    
    that, given an integer N and two non-empty zero-indexed arrays P and Q consisting of M integers, returns an array consisting of M elements specifying the consecutive answers to all the queries.
    
    For example, given an integer N = 26 and arrays P, Q such that:
    
        P[0] = 1    Q[0] = 26
        P[1] = 4    Q[1] = 10
        P[2] = 16   Q[2] = 20
    the function should return the values [10, 4, 0], as explained above.
    
    Assume that:
    
    N is an integer within the range [1..50,000];
    M is an integer within the range [1..30,000];
    each element of arrays P, Q is an integer within the range [1..N];
    P[i] ≤ Q[i].
    Complexity:
    
    expected worst-case time complexity is O(N*log(log(N))+M);
    expected worst-case space complexity is O(N+M), beyond input storage (not counting the storage required for input arguments).
    Elements of input arrays can be modified.

--- src/com/codility/CountSemiprimes.java
+++ src/com/codility/CountSemiprimes.java
@@ -1,85 +1,64 @@
 package com.codility;
 
-import java.util.Arrays;
-
 /**
  * @author Anatoly Chernysh
  */
 public class CountSemiprimes {
 
     public static void main(String []args) {
-        System.out.println(factorization(26, factorizations(26)));
+        solution(26, new int [] {1, 4, 16}, new int [] {26, 10, 20});
     }
 
     public static int[] solution(int N, int[] P, int[] Q) {
-        return null;
+        boolean []notPrimes = findNotPrimes(N);
+        boolean []semiPrimes = findSemiPrimes(N, notPrimes);
+        int []result = new int[P.length];
+
+        int []sum = new int[N + 1];
+        int index = 0;
+        for (int i = 1; i <= N; i++) {
+            sum[i] = sum[i - 1] + (semiPrimes[i] ? 1 : 0);
+            while (index < Q.length && i >= Q[index]) {
+                result[index] = sum[Q[index]] - sum[P[index] - 1];
+                index++;
+            }
+        }
+
+        return result;
     }
 
-    public static void findPrimes(int N) {
-        boolean []sieve = new boolean[N + 1];
-        Arrays.fill(sieve, true);
+    public static boolean[] findNotPrimes(int N) {
+        boolean []notPrimes = new boolean[N + 1];
 
-        sieve[0] = false;
-        sieve[1] = false;
+        notPrimes[0] = true;
+        notPrimes[1] = true;
 
         int i = 2;
         while (i * i <= N) {
-            if (sieve[i]) {
+            if (!notPrimes[i]) {
                 int k = i * i;
                 while (k <= N) {
-                    sieve[k] = false;
+                    notPrimes[k] = true;
                     k += i;
                 }
             }
             i++;
         }
-
-        print(sieve);
+        return notPrimes;
     }
 
-    public static int[] factorizations(int N) {
-        int []factorizations = new int[N + 1];
-        Arrays.fill(factorizations, 0);
-
-        int i = 2;
-        while (i * i <= N) {
-            if (factorizations[i] == 0) {
-                int k = i * i;
-                while (k <= N) {
-                    if (factorizations[i] == 0) {
-                        factorizations[k] = i;
+    public static boolean[] findSemiPrimes(int N, boolean []notPrimes) {
+        boolean []semiPrimes = new boolean[N + 1];
+        for (int i = 2;i * i <= N;i++) {
+            if (!notPrimes[i]) {
+                semiPrimes [i * i] = true;
+                for (int k = i * (i + 1);k <= N;k += i) {
+                    if (!notPrimes[k / i]) {
+                        semiPrimes[k] = true;
                     }
-                    k += i;
                 }
             }
-            i++;
         }
-
-        print(factorizations);
-        return factorizations;
-    }
-
-    public static int factorization(int x, int []factorizations) {
-        int primeFactors = 0;
-        while (factorizations[x] > 0) {
-            primeFactors += factorizations[x];
-            x = x / factorizations[x];
-        }
-        primeFactors += x;
-        return primeFactors;
-    }
-
-    public static void print(boolean []sieve) {
-        for (int i = 0;i < sieve.length;i++) {
-            if (sieve[i]) {
-                System.out.print(i + " ");
-            }
-        }
-    }
-
-    public static void print(int []factorizations) {
-        for (int i = 0;i < factorizations.length;i++) {
-            System.out.print(i + "=" + factorizations[i] + " ");
-        }
+        return semiPrimes;
     }
 }
