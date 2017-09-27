c3a4ff2fd13f642bba8dc5f12832aa37fe7b76e2
Anatoly Chernysh <achernish@arterra.com>
2017-09-26 20:40:47 -0400

    Find permutations for N and for string

--- src/com/experitest/FindPermutations.java
+++ src/com/experitest/FindPermutations.java
@@ -13,10 +13,10 @@ public class FindPermutations {
     private Set<List<Integer>> allPermutations = new HashSet<>();
 
     public static void main(String[] args) {
-        int n = 4;
-
         FindPermutations findPermutations = new FindPermutations();
-        findPermutations.findPermutations(n);
+        //findPermutations.findPermutations(4);
+
+        System.out.println(findPermutations.findPermutations("abc"));
     }
 
     public void findPermutations(int n) {
@@ -39,5 +39,35 @@ public class FindPermutations {
                 newList.remove(j);
             }
         }
+    }
+
+    public List<String> findPermutations(String str) {
+        if (str == null) {
+            return null;
+        }
+
+        List<String> permutations = new ArrayList<>();
+        if (str.length() == 0) {
+            permutations.add("");
+            return permutations;
+        }
+
+        char first = str.charAt(0);
+        String reminder = str.substring(1);
+        List<String> words = findPermutations(reminder);
+        for (String word : words) {
+            for (int i = 0;i <= word.length();i++) {
+                String s = insertCharAt(word, first, i);
+                permutations.add(s);
+            }
+        }
+
+        return permutations;
+    }
+
+    private String insertCharAt(String word, char first, int index) {
+        String start = word.substring(0, index);
+        String end = word.substring(index);
+        return start + first + end;
     }
 }
