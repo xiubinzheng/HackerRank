bb464354705ca29e90d79e8c13ad70c0ae03f22d
Anatoly Chernysh <achernish@arterra.com>
2018-06-19 19:10:52 -0400

    387. First Unique Character in a String

---
+++ src/com/leetcode/FirstUniqCharInStr.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+/**
+ * @author Anatoly Chernysh
+ */
+public class FirstUniqCharInStr {
+
+    public static void main(String[] args) {
+        //System.out.println(firstUniqChar("leetcode"));
+        System.out.println(firstUniqChar("loveleetcode"));
+    }
+
+    public static int firstUniqChar(String s) {
+        int alphabet[] = new int[26];
+
+        for (int i = 0;i < s.length();i++) {
+            alphabet[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0;i < s.length();i++) {
+            if (alphabet[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+}
