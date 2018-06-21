8b646d0b3ed330f39622447489976ccc2c7c4fb8
Anatoly Chernysh <achernish@arterra.com>
2018-06-20 21:47:52 -0400

    33. Search in Rotated Sorted Array*

---
+++ src/com/leetcode/SearchRotatedSortedArray.java
@@ -0,0 +1,45 @@
+package com.leetcode;
+
+/**
+ * @author Anatoly Chernysh
+ */
+public class SearchRotatedSortedArray {
+
+    public static void main(String[] args) {
+        int []nums = {4,5,6,7,0,1,2};
+        System.out.println(search(nums, 0));
+    }
+
+    public static int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midVal = nums[mid];
+
+            if (midVal == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+}
