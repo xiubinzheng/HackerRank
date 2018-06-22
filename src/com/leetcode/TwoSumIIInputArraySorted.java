e4dc52cddf28099e0df8f62e69b1334a40327c46
Anatoly Chernysh <achernish@arterra.com>
2018-06-22 18:36:06 -0400

    167. Two Sum II - Input array is sorted

--- src/com/leetcode/TwoSumIIInputArraySorted.java
+++ src/com/leetcode/TwoSumIIInputArraySorted.java
@@ -5,7 +5,17 @@ package com.leetcode;
  */
 public class TwoSumIIInputArraySorted {
 
-    public int[] twoSum(int[] numbers, int target) {
+    public static void main(String[] args) {
+        //int []numbers = {1,2,7,11,12,15}; // 9
+        int []numbers = {2,7,11,15};
+
+        int []result = twoSum(numbers, 9);
+        for (int value : result) {
+            System.out.println(value);
+        }
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
         int lo = 0, hi = numbers.length - 1;
         while (lo <= hi) {
             int sum = numbers[lo] + numbers[hi];
@@ -21,8 +31,8 @@ public class TwoSumIIInputArraySorted {
         }
 
         int result[] = new int[2];
-        result[0] = lo;
-        result[1] = hi;
+        result[0] = lo + 1;
+        result[1] = hi + 1;
         return result;
     }
 }
